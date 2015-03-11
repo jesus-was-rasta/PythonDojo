e36615fa8c37ff9e5bc40b9b69e243e351add259
jesuswasrasta <jesus_was_rasta@yahoo.it>
2015-03-11 22:58:51 +0100

    Clean experiments

--- HelloWorld/HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld/HelloWorld.py
@@ -1,3 +1,3 @@
 firstName = input("What's your first name? ")
 lastName = input("What's your last name? ")
-print("Hello ".swapcase() + firstName.upper() + " " + lastName.lower() + "!")
+print("Hello " + firstName + " " + lastName + "!")
