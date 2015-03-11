9a92670641ac4fd5acb7eecd994b53fa413c92d5
jesuswasrasta <jesus_was_rasta@yahoo.it>
2015-03-11 22:56:25 +0100

    upper() and lower() and swapcase()

--- HelloWorld/HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld/HelloWorld.py
@@ -1,3 +1,3 @@
 firstName = input("What's your first name? ")
 lastName = input("What's your last name? ")
-print("Hello " + firstName + " " + lastName + "!")
+print("Hello ".swapcase() + firstName.upper() + " " + lastName.lower() + "!")
