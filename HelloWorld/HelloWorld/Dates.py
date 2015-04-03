185aff76c9e30d3476c7615fda1600b8253f9745
jesuswasrasta <jesus_was_rasta@yahoo.it>
2015-04-03 22:09:11 +0200

    Next birthday exercise: don't know how to create a datetime variable! :(

--- HelloWorld/HelloWorld/Dates.py
+++ HelloWorld/HelloWorld/Dates.py
@@ -7,5 +7,9 @@ import datetime
 currentDate = datetime.date.today()
 
 #formatting dates
-print (currentDate.strftime('%d %m %Y'))
+#print (currentDate.strftime('%d %m %Y'))    # 03 04 2015
+#print (currentDate.strftime('%A %d %B %Y')) #Friday 03 April 2015
 
+birthDateString = input ("What is your birth date (dd-mm-yyyy)?")
+birthDate = datetime.datetime.strptime(birthday, "%d-%m-%Y").date()
+#nextBirthday = ?
