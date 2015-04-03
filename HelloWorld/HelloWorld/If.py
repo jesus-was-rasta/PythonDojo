77dd184c6f83edeee910a1c3444d07ede70358d4
jesuswasrasta <jesus_was_rasta@yahoo.it>
2015-04-03 22:30:46 +0200

    elif

--- HelloWorld/HelloWorld/If.py
+++ HelloWorld/HelloWorld/If.py
@@ -3,6 +3,8 @@
 expressDelivery = input("Do you want express delivery?")
 if expressDelivery == 'y':
     print ("There will be an extra charge!")
+elif expressDelivery == 'Y':
+    print ("There will be an extra charge!")
 else:
     print ("No extra charge")
-
+print ("Have a nice day :)")
