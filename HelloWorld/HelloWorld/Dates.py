826a9b0f52a5008bff9a7cde1162496a380361c6
jesuswasrasta <jesus_was_rasta@yahoo.it>
2015-04-03 21:52:23 +0200

    datetime exercises

---
+++ HelloWorld/HelloWorld/Dates.py
@@ -0,0 +1,11 @@
+#Exercise: days until my birthday
+
+#importing class datetime
+import datetime
+
+#today
+currentDate = datetime.date.today()
+
+#formatting dates
+print (currentDate.strftime('%d %m %Y'))
+
--- HelloWorld/HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld/HelloWorld.py
@@ -2,6 +2,9 @@
 #lastName = input("What's your last name? ")
 #print("Hello " + firstName + " " + lastName + "!")
 
+message = "ciao"
+print(message.count('c'))
+
 lenght = 23
 height = 12
 area = lenght * height
