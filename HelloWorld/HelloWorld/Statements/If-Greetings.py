f5aceefc97002fbb086f167e7ef46dc2e5632969
jesuswasrasta <jesus_was_rasta@yahoo.it>
2015-04-03 22:33:58 +0200

    Greetings excercise

---
+++ HelloWorld/HelloWorld/Statements/If-Greetings.py
@@ -0,0 +1,10 @@
+#Greetings
+country = input("Where do you come from?")
+if country == 'ENGLAND':
+    print ("Hello")
+elif country == 'ITALIA':
+    print ("Ciao")
+elif country == 'FRANCE':
+    print ("Bonjour")
+else:
+    print ("Aloha")
