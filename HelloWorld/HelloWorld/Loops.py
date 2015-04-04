85683e7d76fc059361b70c599d9f8bb367be7430
jesuswasrasta <jesus_was_rasta@yahoo.it>
2015-04-04 17:09:03 +0200

    Loops

---
+++ HelloWorld/HelloWorld/Loops.py
@@ -0,0 +1,16 @@
+#Loops
+
+for steps in range (4):
+    print(steps)
+
+#parte a contare da 1
+for steps in range (1,4):
+    print(steps)
+
+#parte a contare da 1, prende un valore ogni 2, vengono quindi saltati i numeri pari
+for steps in range (1,10,2):
+    print(steps)
+
+#gli do io i valori
+for steps in range [1,2,5,7]:
+    print(steps)
---
+++ HelloWorld/HelloWorld/Turtle.py
@@ -0,0 +1,8 @@
+#Turtle
+import turtle
+
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(100)
+turtle.right(45)
+turtle.color('pink')
